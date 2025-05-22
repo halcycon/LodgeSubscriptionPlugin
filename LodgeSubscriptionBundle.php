da8194411c9031e570f19a5370805b8510c44be9
halcycon <adam@thecuckoocamp.co.uk>
2025-05-22 20:03:54 +0100

    Refactor LodgeSubscriptionBundle by consolidating plugin functionality, removing obsolete files, and implementing schema creation on plugin install and update.

--- LodgeSubscriptionBundle.php
+++ LodgeSubscriptionBundle.php
@@ -2,6 +2,9 @@
 namespace MauticPlugin\LodgeSubscriptionPlugin;
 
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
+use Mautic\PluginBundle\Entity\Plugin;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class LodgeSubscriptionBundle extends PluginBundleBase
@@ -13,28 +16,26 @@ class LodgeSubscriptionBundle extends PluginBundleBase
     {
         parent::build($container);
     }
-
+    
     /**
-     * {@inheritdoc}
+     * Called when plugin is installed
      */
-    public function getParent()
+    public static function onPluginInstall(Plugin $plugin, $em, $schema)
     {
-        return null;
+        $tool = new SchemaTool($em);
+        $entities = [
+            $em->getClassMetadata('MauticPlugin\LodgeSubscriptionPlugin\Entity\SubscriptionRate'),
+            $em->getClassMetadata('MauticPlugin\LodgeSubscriptionPlugin\Entity\Payment')
+        ];
+
+        $tool->createSchema($entities);
     }
 
     /**
-     * {@inheritdoc}
+     * Called when plugin is updated
      */
-    public function getPath()
+    public static function onPluginUpdate(Plugin $plugin, $em, $schema, $fromVersion)
     {
-        return __DIR__;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getName()
-    {
-        return 'LodgeSubscriptionBundle';
+        // Handle plugin updates here
     }
 } 
