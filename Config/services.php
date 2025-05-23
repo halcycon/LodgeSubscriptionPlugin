9414bfda8f08f5aa85dc7d945e21b72e391c4d53
halcycon <adam@thecuckoocamp.co.uk>
2025-05-23 19:25:05 +0100

    Fix typo in service exclusion paths in services.php to ensure accurate autowiring configuration.

--- Config/services.php
+++ Config/services.php
@@ -16,7 +16,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // Register all classes in the bundle for autowiring
     $services->load('MauticPlugin\\LodgeSubscriptionBundle\\', '../')
         ->exclude([
-            '../{Config,DependencyInjection,Entity,Migrations,Tests}',
+            '../{Config,DependencyInjection,Entity,Migration,Tests}',
             '../*Bundle.php',
         ]);
 
